3ec869da92a04adacbd9c6c15deab4edda0a7a5f
Marlon Lima <marlonlimax2012@gmail.com>
2023-07-28 19:15:24 -0300

    creating form component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import Banner from './components/Banner';
-import TextField from './components/TextField';
+import Formulario from './components/Formulario';
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <TextField label="Nome" placeholder="Digite seu nome" />
-      <TextField label="Cargo" placeholder="Digite seu cargo" />
-      <TextField label="Imagem" placeholder="Digite o endereço da imagem" />
+      <Formulario />
     </div>
   );
 }
---
+++ src/components/Formulario/index.js
@@ -0,0 +1,17 @@
+import TextField from '../TextField'
+import './Formulario.css'
+
+const Formulario = () => {
+    return (
+        <section className="formulario">
+            <form >
+                <h2>Preencha os dados para criar o card do colaborador</h2>
+                <TextField label="Nome" placeholder="Digite seu nome" />
+                <TextField label="Cargo" placeholder="Digite seu cargo" />
+                <TextField label="Imagem" placeholder="Digite o endereço da imagem" />
+            </form>
+        </section>
+    )
+}
+
+export default Formulario
