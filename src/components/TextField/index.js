f1692ea4d84c9f98fe569f2382f43a45ab924c98
Marlon Lima <marlonlimax2012@gmail.com>
2023-07-30 17:57:44 -0300

    creating user input

--- src/components/TextField/index.js
+++ src/components/TextField/index.js
@@ -1,10 +1,18 @@
 import './TextField.css'
 
 const TextField = (props) => {
+
+    let value = 'Limax Silva'
+
+    const whenTyped = (event) => {
+        value = event.target.value
+        console.log(value)
+    }
+
     return (
         <div className="text-field">
             <label>{props.label}</label>
-            <input required={props.mandatory} placeholder={props.placeholder} />
+            <input value={value} onChange={whenTyped} required={props.mandatory} placeholder={props.placeholder} />
         </div>
     )
 }
