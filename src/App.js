bf3844933d3020675dc1c38f188a39175311db94
Marlon Lima <marlonlimax2012@gmail.com>
2023-08-01 19:46:05 -0300

    Mapping the collaborators

--- src/App.js
+++ src/App.js
@@ -55,7 +55,14 @@ function App() {
       <Banner />
       <Formulario times={times.map(time => time.nome)} theRegisteredEmployee={colaborador => toTheNewContributorAdded(colaborador)} />
 
-      {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria} />)}
+      {times.map(time => <Time
+        key={time.nome}
+        nome={time.nome}
+        corPrimaria={time.corPrimaria}
+        corSecundaria={time.corSecundaria}
+        colaboradores={colaboradores.filter(colaborador => colaborador.time == time.nome)}
+
+      />)}
 
 
 
--- src/components/collaborator/index.js
+++ src/components/collaborator/index.js
@@ -1,14 +1,14 @@
 import './Collaborator.css'
 
-const Colaborador = () => {
+const Colaborador = ({ nome, imagem, cargo }) => {
     return (
         <div className='colaborador'>
             <div className='cabecalho'>
-                <img src='https://github.com/Marlon-Limax.png' alt='Limax Teste' />
+                <img src={imagem} alt={nome} />
             </div>
             <div className='rodape'>
-                <h4>Límax Teste</h4>
-                <h5>Desenvolvedor</h5>
+                <h4>{nome}</h4>
+                <h5>{cargo}</h5>
             </div>
         </div>
     )
