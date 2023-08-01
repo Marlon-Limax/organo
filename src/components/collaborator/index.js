142cc4418ca0021fb45047c89941c649600a7d81
Marlon Lima <marlonlimax2012@gmail.com>
2023-07-31 21:32:21 -0300

    Creating the Collaborator component and Card

---
+++ src/components/collaborator/index.js
@@ -0,0 +1,17 @@
+import './Collaborator.css'
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/Marlon-Limax.png' alt='Limax Teste' />
+            </div>
+            <div className='rodape'>
+                <h4>Límax Teste</h4>
+                <h5>Desenvolvedor</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
